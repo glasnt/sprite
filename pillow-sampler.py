7a9cd7e52e9bb5a40eef07dbbd528e3209efe461
Katie McLaughlin <katie@glasnt.com>
2017-12-01 16:38:03 +0100

    ⚠️

--- pillow-sampler.py
+++ pillow-sampler.py
@@ -3,14 +3,12 @@
 import sys
 from PIL import Image
 
-
 im = Image.open(sys.argv[1])
 im = im.convert('RGB')
 im = im.transpose(Image.FLIP_TOP_BOTTOM).transpose(Image.ROTATE_270)
 
-print("<div><code>")
+print("<div class='palette'><code>")
 histo = im.getcolors()
-
 
 # if sampling fails, drop count https://stackoverflow.com/questions/1065945/how-to-reduce-color-palette-with-pil
 # using 32 here as POC
@@ -35,10 +33,11 @@ def rgb2hex(pix):
 histo = im.getcolors()
 chart = {}
 
-#stars = "▷◼︎✤✷✽❤︎✰❐❄︎➤⬆︎⬇︎♠︎♣︎♥︎♦︎♞♟✝︎☘︎"
+stars = "▷◼︎✤✷✽❤︎✰❐❄︎➤⬆︎⬇︎♠︎♣︎♥︎♦︎♞♟✝︎☘︎"
 # TODO - monospace friendly stars
 #stars = "abcdefghijklmnopqrstuvwxyz"
-stars = "███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████"
+#stars = "abc
+#stars = "███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████"
 
 if len(sys.argv) == 3:
     step = int(sys.argv[2])
@@ -47,14 +46,33 @@ else:
 
 for idx, x in enumerate(histo):
     h = rgb2hex(x[1])
-    chart[h] = stars[idx]
+    chart[h] = stars[idx % len(stars)]
 
+style = """
+<style>
+table {
+    border-collapse: collapse;
+}
 
-print("<div style='line-height: 0.7'><code>")
+table, th, td {
+    border: 1px solid black;
+}
+
+td { 
+    vertical-align: middle;
+    text-align: center;
+    height: 30px;
+    width: 30px
+}
+</style>
+"""
+print(style)
+print("<table  class='chart'>")
 for x in range(0, im.width, step):
+    print("<tr>")
     for y in range(0, im.height, step):
         p = rgb2hex(im.getpixel((x,y))) 
-        print("<span style='color: {}'>{}</span>".format(p, chart[p]), end="")
-    print("<br>")
+        print("<td style='color: {}'>{}</td>".format(p, chart[p]), end="")
+    print("</tr>")
     #print("")
-print("</div>")
+print("</table>")
